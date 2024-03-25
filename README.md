389e2f690c6235948e1d0ce624a1466d62864938
uk <47803499+SongCastle@users.noreply.github.com>
2024-03-25 10:20:33 +0900

    Add MySQL Compose

--- README.md
+++ README.md
@@ -3,3 +3,4 @@
 Compose Templates for me
 
 - [postgres](https://github.com/SongCastle/compose-templates/tree/main/postgres)
+- [mysql](https://github.com/SongCastle/compose-templates/tree/main/mysql)
---
+++ mysql/docker-entrypoint-initdb.d/init-db.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -e
+
+exec_sql() {
+  dbName=$1
+
+  mysql -u 'root' -p"$MYSQL_ROOT_PASSWORD" <<-EOSQL
+		GRANT ALL ON \`$dbName\`.* TO '$MYSQL_USER'@'%';
+	EOSQL
+  mysql -u "$MYSQL_USER" -p"$MYSQL_PASSWORD" -D "$dbName" < '/docker-entrypoint-initdb.d/schema.sql.template'
+}
+
+exec_sql "$MYSQL_DATABASE"
+
+if [ ! -z "$MYSQL_CUSTOM_DATABASE" ]; then
+  mysql -u 'root' -p"$MYSQL_ROOT_PASSWORD" <<-EOSQL
+		CREATE DATABASE IF NOT EXISTS \`$MYSQL_CUSTOM_DATABASE\` CHARACTER SET utf8mb4 COLLATE utf8mb4_bin;
+	EOSQL
+  exec_sql "$MYSQL_CUSTOM_DATABASE"
+fi
