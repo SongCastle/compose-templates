6bd0416619476e7f073ed61635ac715e994749e2
uk <47803499+SongCastle@users.noreply.github.com>
2024-04-16 09:49:21 +0900

    Add Linter to Express Compose

--- express/app/server.ts
+++ express/app/server.ts
@@ -1,13 +1,14 @@
-import express from 'express'
+import process from 'node:process';
+import express from 'express';
 
-const app = express()
-const host = process.env.EXPRESS_HOST || '';
-const port = Number(process.env.EXPRESS_PORT || 3000);
+const app = express();
+const host = process.env.EXPRESS_HOST ?? '';
+const port = Number(process.env.EXPRESS_PORT ?? 3000);
 
-app.get('/', (_, res) => {
-  res.send('Hello World!')
-})
+app.get('/', (_, response) => {
+  response.send('Hello World!');
+});
 
 app.listen(port, host, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
